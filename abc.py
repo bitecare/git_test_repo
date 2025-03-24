46270bcb5bd0d2e91e487e898ddc946f94bc113a
bitecare <caca2141@naver.com>
2025-03-24 23:58:56 +0900

    add abc.py

---
+++ abc.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import copy
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+
+from tqdm.auto import tqdm
+
+
+
+def add(x, y):
+    return x + y
